bbc79028df8defa59997bf55c66ea4feb22ba655
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-09-05 18:47:34 +0800

    lr banner ok

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(sassMiddleware({
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true,
-  //debug: true
+  debug: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
